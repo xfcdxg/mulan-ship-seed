f54d7b020ac6a929f2f0d1d009827ba225b4911c
xfcdxg <xfcdxg_zq@163.com>
2018-06-23 01:55:16 +0800

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,18 @@ module.exports = {
   apps : [
     // First application
     {
-      name            : "mulan-ship-seed",
-      watch           : true,
-      script          : "./server",
-      interpreter     : './node_modules/.bin/babel-node',
-      log_date_format : 'MM-DD HH:mm:ss',
-      merge_logs      : true,
-      ignore_watch    : ['.project'],
+      name             : "mulan-ship-seed",
+      watch            : true,
+      script           : "./server",
+      interpreter      : './node_modules/.bin/babel-node',
+      log_date_format  : 'MM-DD HH:mm:ss',
+      merge_logs       : true,
+      ignore_watch     : ['.project'],
       env: {
         COMMON_VARIABLE: "true"
       },
       env_production   : {
-        NODE_ENV: "production"
+        NODE_ENV       : "production"
       }
     }
   ]
